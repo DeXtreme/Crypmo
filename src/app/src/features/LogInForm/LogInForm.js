ba348f67837653b07768a4f181c5bcfe45c7d913
DeXtreme <dbakolbire@gmail.com>
2022-01-19 14:37:12 +0000

    feature(LogInForm): Create LogInForm feature
    Creates the basic LogInForm feature and files and includes it
    on the LogIn page

---
+++ src/app/src/features/LogInForm/LogInForm.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+
+function LogInForm(props){
+    return (
+        <div>LoginForm works</div>
+    );
+}
+
+LogInForm.propTypes = {
+    className : PropTypes.string
+}
+
+export default LogInForm;
+
--- src/app/src/pages/LogIn/LogIn.js
+++ src/app/src/pages/LogIn/LogIn.js
@@ -1,10 +1,11 @@
 import Navbar from '../../layouts/Navbar/Navbar';
+import LogInForm from '../../features/LogInForm';
 
 export function LogIn(props){
     return (
         <div>
             <Navbar fixed={false} showMenu={false}/>
-            
+            <LogInForm />
         </div>
     );
 }
